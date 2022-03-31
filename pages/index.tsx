eff16ca84cc97de797296866b74466fc65413dec
vhalvarez <alvarez.programmer@gmail.com>
2022-03-31 11:07:06 -0400

    Estructura de directorios y ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { ShopLayout } from "../components/layouts";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <h1>Hola mundo</h1>
-  )
-}
+    return (
+        <ShopLayout
+            title={"Teslo - Shop"}
+            pageDescription={"Encuentra los mejores prodductos de Teslo aqui"}
+        >
+            <Typography variant="h1" component="h1">
+                Tienddad
+            </Typography>
+            <Typography variant="h2" sx={{ mb: 1 }}>
+                Todos los productos
+            </Typography>
+        </ShopLayout>
+    );
+};
 
-export default Home
+export default Home;
