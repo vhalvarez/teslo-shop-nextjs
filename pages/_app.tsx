d7b7ce88af37ffba81e918f1a1765cf7943dff94
vhalvarez <alvarez.programmer@gmail.com>
2022-04-07 17:22:08 -0400

    Custom Hooks y configuracion Global SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import { lightTheme } from "../themes/";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={lightTheme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <SWRConfig
+            value={{
+                // refreshInterval: 3000,
+                fetcher: (resource, init) =>
+                    fetch(resource, init).then((res) => res.json()),
+            }}
+        >
+            <ThemeProvider theme={lightTheme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </SWRConfig>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,10 @@ import type { NextPage } from "next";
 import { Typography } from "@mui/material";
 import { ShopLayout } from "../components/layouts";
 import { ProductList } from "../components/products/";
-
-import useSWR from 'swr'
-const fetcher = (...args: [key:string]) => fetch(...args).then(res => res.json())
+import { useProducts } from "../hooks";
 
 const HomePage: NextPage = () => {
-
-    const { data, error } = useSWR('/api/products', fetcher)
-
-    if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+    const { products, isLoading, isError } = useProducts("/products");
 
     return (
         <ShopLayout
@@ -25,7 +19,11 @@ const HomePage: NextPage = () => {
                 Todos los productos
             </Typography>
 
-            <ProductList products={ data } />
+            {isLoading ? (
+                <h1>Cargando...</h1>
+            ) : (
+                <ProductList products={products} />
+            )}
         </ShopLayout>
     );
 };
